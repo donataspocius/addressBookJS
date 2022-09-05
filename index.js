4ca2dc115fb3c2be2b94a99e83d44fc84ddd5483
Donatas Pocius <pocius.coding@gmail.com>
2022-09-06 00:59:48 +0300

    commit v1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello, world");
+let contacts = [];
+
+document.querySelector("#input-data").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let contactInput = Object.fromEntries(new FormData(e.target));
+  contacts.push(contactInput);
+  window.localStorage.setItem("contacts", JSON.stringify(contacts));
+
+  renderContacts();
+});
+
+function renderContacts() {
+  // if ()
+  let outputData = document.createElement("div");
+  outputData.className = "outputContainer";
+  document.body.append(outputData);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  let loadedData = JSON.parse(window.localStorage.getItem("contacts"));
+  renderContacts();
+});
+
+// function createElement(tag, props) {
+//   let newTag = document.createElement(tag);
+//   if (props && props.length) {
+//     newTag[props.name] = props.value;
+//   }
+//   return newTag;
+// }
